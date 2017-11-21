184a95589e3ccca845db604fe5298469621c3068
Marcelo Choque <casiprogramador@gmail.com>
2017-11-21 17:58:00 -0400

    datatable en saldo de vacacion

--- public/js/data-table.js
+++ public/js/data-table.js
@@ -5,6 +5,17 @@
         "aLengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
         "iDisplayLength": 5
       });
+      $('.data-table-print').DataTable(
+        {
+          dom: 'Bfrtip',
+          buttons: [
+              'copyHtml5',
+              'excelHtml5',
+              'csvHtml5',
+              'pdfHtml5'
+          ]
+        }
+      );
     });
   })(jQuery);
   
