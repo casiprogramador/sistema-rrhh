3f938cfb4728fda4b9222e894f30afbbd560a878
Marcelo Choque <casiprogramador@gmail.com>
2017-11-25 13:20:57 -0400

    Limpiado de saldo vacacion

--- routes/vacacion/saldo.js
+++ routes/vacacion/saldo.js
@@ -9,9 +9,7 @@ router.get('/formulario', function(req, res, next) {
     modelos.Area.findAll({
         attributes: ['id', 'desc_area']
     }).then(areas => {
-        //console.log(JSON.stringify(areas));
-        //res.cookie('cokie' , 'cookie_value').send(areas);
-        //console.log("Cookies :  ", req.cookies);
+
         modelos.sequelize.query('SELECT e.ndi, e.materno, e.paterno, e.nombres, a.cargo, c.fecha_inicio, s.gestion, s.dias , r.desc_area FROM public."Empleados" e, public."Contratos" c, public."Cargos" a , public."Areas" r, public."Saldo_Vacacions" s WHERE e.id=c.id_empleado and c.id_cargo=a.id and a.id_area=r.id and s.id_empleado = e.id ORDER BY r.desc_area, e.ndi').spread((results, metadata) => {
             //console.log(JSON.stringify(results));
             res.render('vacacion/saldo', { areas: areas, saldovacaciones: results, moment:moment });
