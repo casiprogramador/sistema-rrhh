e6745bd00f20d507d6e69febfbb0bb143fe4c662
Marcelo Choque <casiprogramador@gmail.com>
2017-11-27 21:38:10 -0400

    FInalizacion de administracion de boletas

--- routes/administrar/boleta.js
+++ routes/administrar/boleta.js
@@ -3,9 +3,15 @@ var router = express.Router();
 var modelos = require('../../models/index');
 var Sequelize = require('sequelize');
 var moment = require('moment');
-
+/**
+ * Vista principal de administracion de boletas 
+ * Administrador
+ */
 router.get('/',function(req, res, next) {
   modelos.Boleta.findAll({
+    where: {
+      estado: 'Pendiente',
+    },
     include: ['empleado','tipoboleta']
 
  }).then((boletas) => {
@@ -54,7 +60,11 @@ router.post('/buscar', (req, res) => {
   **/
 
   if(req.body.estado == 'Todos'){
-    res.redirect('/administrar/boleta');
+    modelos.Boleta.findAll({
+      include: ['empleado','tipoboleta'],
+    }).then((boletas) => {
+      res.render('administrar/boleta', { boletas: boletas, moment:moment });
+    })
   }else{
     modelos.Boleta.findAll({
       where: {
@@ -62,10 +72,50 @@ router.post('/buscar', (req, res) => {
       },
       include: ['empleado','tipoboleta'],
     }).then((boletas) => {
-      //console.log('\x1b[33m%s\x1b[0m: ',JSON.stringify(boletas));
+      
       res.render('administrar/boleta', { boletas: boletas, moment:moment });
     })
   }
 
+});
+
+router.post('/estado', (req, res) => {
+ console.log('\x1b[33m%s\x1b[0m: ',JSON.stringify(req.body));
+ let actualizarBoleta = { estado: req.body.estado_boleta, observacion: req.body.observacion,usuario_anulacion_aprobacion:res.locals.user.id, fecha_anulacion_aprobacion:moment().format() };
+ modelos.Boleta.update(actualizarBoleta, { where: { id: req.body.id_boleta } }).then((updateboleta) => {
+
+     
+     modelos.Saldo_Vacacion.findOne({
+      where: {
+        id_empleado: req.body.id_empleado,
+        prescrito_estado: false
+      },
+      order: [
+        ['gestion', 'ASC'],
+      ]
+      }).then((saldovacacion) => {
+           //res.render('administrar/boleta',{boletas:boletas, moment:moment});
+          
+          var dias_vacacion = req.body.dias_vacacion;
+          var dias_vacacion_saldo = saldovacacion.dias;
+          var dias_vacaciones_actualizar = saldovacacion.dias;
+          
+          if(req.body.estado_boleta == 'Aprobado'){
+            var dias_vacaciones_actualizar = dias_vacacion_saldo-dias_vacacion;
+            console.log('\x1b[33m%s\x1b[0m: ','DIAS:'+dias_vacaciones_actualizar);
+            
+          }
+          
+
+          let updateValues = { dias: dias_vacaciones_actualizar};
+          
+          modelos.Saldo_Vacacion.update(updateValues, { where: { id: saldovacacion.id } }).then((result) => {
+    
+              console.log(result);
+              req.flash('success_msg','Boleta actualizada correctamene');
+              res.redirect('/administrar/boleta')
+          });
+      })
+ });
 });
 module.exports = router;
