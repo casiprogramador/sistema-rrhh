8487c250e06965b3a7f9476d94ea652f5587c67d
Stephanie Luna Quevedo <stephanielunaquevedo@gmail.com>
2017-12-07 14:44:35 -0400

    se termino las interfaces de horario y asignacion de horario

---
+++ public/js/horarios.js
@@ -0,0 +1,53 @@
+(function($) {
+
+    $('#table-horarios').DataTable({
+        "order": [
+            [3, "desc"]
+        ],
+        language: {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    });
+    $( ".button-opciones" ).on( "click", function() {
+        var id_boleta = $(this).attr('id-boleta');
+        var id_empleado = $(this).attr('id-empleado');
+        var nombre = $(this).attr('nombre');
+        var boleta = $(this).attr('boleta');
+        var fecha = $(this).attr('fecha');
+        var dias = $(this).attr('dias');
+
+        $('#modal-empleado-nombre').val(nombre);
+        $('#modal-empleado-fecha').val(fecha);
+        $('#modal-empleado-boleta').val(boleta);
+        $('#modal-empleado-dias').val(dias);
+        $('#input-id-boleta').val(id_boleta);
+        $('#input-id-empleado').val(id_empleado);
+
+    });
+
+    $( "#button-boleta" ).on( "click", function() {
+        $("#form-aprobar-boleta").submit();
+    });
+
+})(jQuery);
---
+++ routes/horario/lista_asignacion_horario.js
@@ -0,0 +1,134 @@
+var express = require('express');
+var router = express.Router();
+var modelos = require('../../models/index');
+var Sequelize = require('sequelize');
+var md_auth = require('../../middleware/authenticated');
+var moment = require('moment');
+/**
+ * Vista principal de administracion de boletas 
+ * Administrador
+ */
+router.get('/', md_auth.ensureAuth,function(req, res, next) {
+  modelos.Boleta.findAll({
+    where: {
+      estado: 'Pendiente',
+    },
+    include: ['empleado','tipoboleta']
+
+ }).then((boletas) => {
+   //console.log('\x1b[33m%s\x1b[0m: ',JSON.stringify(boletas));
+         res.render('horario/lista_asignacion_horario',{boletas:boletas, moment:moment});   
+       })
+});
+
+
+router.post('/editar', (req, res) => {
+  
+    res.render('horario/asignar_horario')
+  
+  
+   
+  });
+
+
+
+
+    
+
+router.post('/buscar', (req, res) => {
+  /**
+  const Op = Sequelize.Op;
+      if(req.body.fecha_inicio<req.body.fecha_fin){
+        if(req.body.estado!='todos'){
+             modelos.Boleta.findAll({
+                 include: ['empleado'],
+                  where: {
+                    estado: req.body.estado,
+                    fecha_solicitud: {
+                      [Op.between]: [req.body.fecha_inicio+" 00:00:00.000 +00:00",req.body.fecha_fin+" 00:00:00.000 +00:00"]
+                  
+                    }}
+              }).then((boletas) => {
+                console.log(boletas);
+                      res.render('administrar/boleta',{boletas:boletas});   
+   
+                    })
+        }
+
+        else{
+          modelos.Boleta.findAll({
+            where: {
+              fecha_solicitud: {
+                [Op.between]: [req.body.fecha_inicio+" 00:00:00.000 +00:00",req.body.fecha_fin+" 00:00:00.000 +00:00"]
+              }  
+            }
+          }).then((boletas) => {
+        res.render('administrar/boleta', { boletas: boletas });
+        })
+        }
+      }
+      else{
+        req.flash('error_msg','La fecha inicio no puede ser mayor a la fecha fin');
+        res.redirect('/administrar/boleta')
+      }
+  **/
+
+  if(req.body.estado == 'Todos'){
+    modelos.Boleta.findAll({
+      include: ['empleado','tipoboleta'],
+    }).then((boletas) => {
+      res.render('administrar/boleta', { boletas: boletas, moment:moment });
+    })
+  }else{
+    modelos.Boleta.findAll({
+      where: {
+        estado: req.body.estado,
+      },
+      include: ['empleado','tipoboleta'],
+    }).then((boletas) => {
+      
+      res.render('administrar/boleta', { boletas: boletas, moment:moment });
+    })
+  }
+
+});
+
+router.post('/estado', (req, res) => {
+ console.log('\x1b[33m%s\x1b[0m: ',JSON.stringify(req.body));
+ let actualizarBoleta = { estado: req.body.estado_boleta, observacion: req.body.observacion,usuario_anulacion_aprobacion:res.locals.user.id, fecha_anulacion_aprobacion:moment().format() };
+ modelos.Boleta.update(actualizarBoleta, { where: { id: req.body.id_boleta } }).then((updateboleta) => {
+
+     
+     modelos.Saldo_Vacacion.findOne({
+      where: {
+        id_empleado: req.body.id_empleado,
+        prescrito_estado: false
+      },
+      order: [
+        ['gestion', 'ASC'],
+      ]
+      }).then((saldovacacion) => {
+           //res.render('administrar/boleta',{boletas:boletas, moment:moment});
+          
+          var dias_vacacion = req.body.dias_vacacion;
+          var dias_vacacion_saldo = saldovacacion.dias;
+          var dias_vacaciones_actualizar = saldovacacion.dias;
+          
+          if(req.body.estado_boleta == 'Aprobado'){
+            var dias_vacaciones_actualizar = dias_vacacion_saldo-dias_vacacion;
+            //console.log('\x1b[33m%s\x1b[0m: ','DIAS:'+dias_vacaciones_actualizar);
+          }
+          
+
+          let updateValues = { dias: dias_vacaciones_actualizar};
+          
+          modelos.Saldo_Vacacion.update(updateValues, { where: { id: saldovacacion.id } }).then((result) => {
+    
+              console.log(result);
+              req.flash('success_msg','Boleta actualizada correctamene');
+              res.redirect('/administrar/boleta')
+          });
+      })
+ });
+});
+module.exports = router;
