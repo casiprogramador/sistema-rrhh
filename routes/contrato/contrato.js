2599fbe5e74f9cadc6323a6649780b15ae957e96
Marcelo Choque <casiprogramador@gmail.com>
2017-12-26 14:42:53 -0400

    Edicion de contrato

--- routes/contrato/contrato.js
+++ routes/contrato/contrato.js
@@ -16,15 +16,31 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.get('/', function(req, res, next) {
+router.get('/editar/:id_contrato', function(req, res, next) {
 
-    modelos.Contrato.findAll({
-       where: { estado: true }, 
-       include: ['cargo','empleado']
-    }).then(contratos => {
-        console.log(JSON.stringify(contratos));
-        res.render('contrato/lista_contratos.ejs',{contratos: contratos, moment:moment });
+    modelos.Contrato.findById(req.params.id_contrato).then(contrato => {
+        modelos.Cargo.findAll().then((cargos) => {
+            res.render('contrato/editar_contrato',{cargos:cargos, contrato: contrato, moment:moment });
+        })
+        
     });
+
+});
+
+router.post('/editar', function(req, res, next) {
+
+    let updateContrato = {
+        nro_contrato: req.body.nro, 
+        fecha_inicio : req.body.fecha_inicio,
+        fecha_fin: req.body.fecha_fin,
+        id_cargo: req.body.cargo
+    };
+    modelos.Contrato.update(updateContrato, { where: { id: req.body.id_contrato } }).then((result) => {
+  
+        req.flash('success_msg','Contrato modificado exitosamente');
+        res.redirect('/contrato');
+
+    })
 
 });
 
