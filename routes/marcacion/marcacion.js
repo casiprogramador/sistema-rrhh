efcadf7f02fb434dff80596e334f4a0b0eb66f7b
richard velasquez <rick_hard_777@hotmail.com>
2017-12-30 12:59:59 -0400

    aa

--- routes/marcacion/marcacion.js
+++ routes/marcacion/marcacion.js
@@ -17,10 +17,10 @@ router.post('/marcaciones', function(req,res2, next) {
   /*var ips="192.168.130.33";
   console.log(ip);*/
     var options = {
-      //host: param.webservice_marcacion.ip,
-      //port: param.webservice_marcacion.puerto,
-      host: "localhost",
-      port: 8080,
+      host: param.webservice_marcacion.ip,
+      port: param.webservice_marcacion.puerto,
+      //host: "localhost",
+      //port: 8080,
       path: '/usr?'+"ip="+ip+"&puerto="+puerto,
       method: 'GET'
     }
@@ -113,10 +113,10 @@ router.post('/guardar_marcacion', function(req,res2, next) {
   /*var ips="192.168.130.33";
   console.log(ip);*/
     var options = {
-      //host: param.webservice_marcacion.ip,
-      //port: param.webservice_marcacion.puerto,
-      host: "localhost",
-      port: 8080,
+      host: param.webservice_marcacion.ip,
+      port: param.webservice_marcacion.puerto,
+      //host: "localhost",
+      //port: 8080,
       path: '/usr2?'+"ip="+ip+"&puerto="+puerto,
       method: 'GET'
     }
